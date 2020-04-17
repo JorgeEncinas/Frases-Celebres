97ffb43a0b3591c941a030dcaa306dc26bfbaacf
Jorge Encinas (Alegre) <jorgecarlosencinas@gmail.com>
2020-04-16 17:20:07 -0700

    Agregado el asistente Pyshtein

--- frases.py
+++ frases.py
@@ -1,5 +1,6 @@
 import csv
 import Levenshtein
+import frases_celebres
 
 
 def carga_csv(archivo):
@@ -13,14 +14,49 @@ def carga_csv(archivo):
         print(e)
     return lista
 
+def normalize(s):
+    replacements = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "i"),
+        ("ó", "o"),
+        ("ú", "u"),
+        (",", ""),
+        (".", ""),
+        ("…", ""),
+        ("¿", ""),
+        ("?", ""),
+        ("!", ""),
+        ("¡", ""),
+        ("«", ""),
+        ("»", ""),
+        (";", ""),
+        (":", ""),
+        ("—", ""),
+    )
+    for a, b in replacements:
+        s = s.replace(a, b).replace(a.upper(), b.upper())
+    return s    
+
+def findme( dfrv, archivo ):
+    dict_tf = dict()
+    for titulo in dfrv.keys():
+        dict_tf[titulo] = []
+    listado = carga_csv(archivo)
+    for linea in listado:
+        if linea[1] in dfrv.keys():
+            if linea[0] not in dfrv[linea[1]]:
+                dict_tf[linea[1]].append(linea[0])
+    return dict_tf
+
 def levDistance(archivo, texto, max_dist):
     listado = carga_csv(archivo)
     lista=[]
-    print("Frase a buscar: %s" % texto)
+    frases_celebres.typing("Frase a buscar: \"%s\"  (￣∇￣*)ゞ" % texto)
     for linea in listado:
         pelicula = linea[1]
         frase = linea[0]
-        distancia = Levenshtein.ratio(frase, texto)
+        distancia = Levenshtein.ratio(normalize(frase.lower()), normalize(texto.lower()))
         if distancia >= max_dist:
             elemento=(distancia, frase, pelicula)
             lista.append(elemento)
--- frases_celebres.py
+++ frases_celebres.py
@@ -15,6 +15,7 @@ import argparse #1 punto
 import csv #puede omitirse gracias a frases.py
 import frases
 import Levenshtein #¿Se usará?
+import time
 
 # 7 pts
 
@@ -22,19 +23,44 @@ def filtrado_frases(listado,frase,limite):      # Funcion de filtrado de frases
         lista=frases.levDistance(listado,frase,limite)
         return lista
 
-
 # 5 pts
 
 def despliega_frases(lista):     # despliegue() que despliega lista de frases filtradas
-    for elemento in lista:
-        print("\"",elemento[1],"\""," - ", elemento[2], ":",elemento[0])
+    #lista_titulos = []
+    dfrv = dict()
+    if len(lista) > 0:
+        typing("Pyshtein: Hemos encontrado una coincidencia en nuestra base de datos !(•̀ᴗ•́)و ̑̑ ")
+        for elemento in lista:
+            #frase_temp = "\"",elemento[1],"\""," - ", elemento[2], ":",elemento[0]
+            frase_temp = "Con coincidencia de {}% -> \" {} \" - {}".format(round(elemento[0]*100, 2), elemento[1], elemento[2])
+            typing(frase_temp)
+            if elemento[2] not in dfrv.keys():
+                dfrv[elemento[2]] = [elemento[1]]
+            else:
+                dfrv[elemento[2]].append(elemento[1])
+    else:
+        typing("Pyshtein: Lo sentimos, no hemos encontrado nada ໒( •́ ∧ •̀ )७ \n")
+    return dfrv
+
+def typing( frase ):
+    for letra in frase:
+        print(letra, end="", flush = True)
+        time.sleep(0.05)
+    print("")
 
 # EXTRA ---- LEAN EL 3 ----------------------------------------------------------
 
 # 2 pts ------ 
 
-def frases_misma_fuente():  # agregar una funcion que obtenga frases célebres de la misma fuente (si existen)
-    return 1                # y cree una nueva lista, que pueda ser desplegada por la función de despliegue()
+def frases_mf( dfrv ):  # agregar una funcion que obtenga frases célebres de la misma fuente (si existen)
+    dict_tf = frases.findme(dfrv, archivo) # y cree una nueva lista, que pueda ser desplegada por la función de despliegue()
+    typing("Pyshtein: Veamos, qué otras frases de los títulos hay (‘•̀ ▽ •́ )✎") 
+    for titulo, lista_frases in dict_tf.items():
+        if len(lista_frases) > 0:
+            typing("Del título {}, he encontrado: ".format(titulo))
+            for frase in lista_frases:
+                typing("\"{}\"".format(frase))
+    typing("\nPyshtein: Ya no hay más ✧*｡٩(ˊᗜˋ*)و✧*｡")          
 
 # 3 pts ---> En lugar de usar funciones, usar objetos (clases y métodos)
 
@@ -42,15 +68,24 @@ def frases_misma_fuente():  # agregar una funcion que obtenga frases célebres d
 
 
 def main( archivo, frase, limite ): #1 punto
+    typing("Pyshtein: ¡Hola, soy Pyshtein!")
     if limite > 1:
         limite = limite/100
-    listaso=filtrado_frases(archivo,frase,limite)               #2 pts por llamarla
-    despliega_frases(listaso)                                   #2 pt por llamarla
+    elif limite < 0:
+        typing("Pyshtein: ¿Qué crees que haces? Ese número no es un porcentaje de algún tipo (　ﾟДﾟ)＜!!")
+    if len(frase) < 1:
+        typing("Pyshtein: ¡No pusiste una frase! ( ‾᷅⚰‾᷄ ) \nInténtalo de nuevo ₍₍ ◝(•̀ㅂ•́)◟ ⁾⁾")
+    else:
+        listaso=filtrado_frases(archivo,frase,limite)               #2 pts por llamarla
+        dfrv = despliega_frases(listaso)                   #2 pt por llamarla
+        if len(dfrv) > 0:
+            frases_mf( dfrv )
+        
 
 if __name__ == "__main__":      #1 punto
     parse =argparse.ArgumentParser()
     parse.add_argument("-a","--archivo",dest="archivo",required=False,default="frases.csv")     #2 pts
-    parse.add_argument("-f", "--frase", dest="frase")                                           #2 pts
+    parse.add_argument("-f", "--frase", dest="frase", required=False, default="")               #2 pts
     parse.add_argument("-l", "--limite", dest="limite", type=float, required = False, default = 0.50)                             #2 pts
     args = parse.parse_args()
     archivo = args.archivo
